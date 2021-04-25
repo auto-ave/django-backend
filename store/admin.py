4e5d1669ae3e98226051787f55296019d6d3a8c5
atpug22 <56637531+atpug22@users.noreply.github.com>
2021-04-25 22:53:40 +0530

    registered store models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Store)
+admin.site.register(Prices)
+admin.site.register(StoreImages)
+admin.site.register(TimeOfWash)
+admin.site.register(Bay)
+admin.site.register(Vehicle)
+admin.site.register(Services)
