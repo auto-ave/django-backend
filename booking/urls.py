17c4edfc80ab5a1769f56bf9927251c64e54481e
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2021-08-03 03:46:16 +0530

    mneedn aa rhai hai

--- booking/urls.py
+++ booking/urls.py
@@ -4,6 +4,6 @@ from booking.views import review, general, slots
 urlpatterns = [
     path('review/', review.ReviewListCreate.as_view()),
     path('booking/<str:booking_id>', general.BookingDetail.as_view()),
-    path('bookings/list/', general.BookingsList.as_view()),
+    path('booking/list/', general.BookingsList.as_view()),
     path('slots/create', slots.SlotCreate.as_view()),
 ]
