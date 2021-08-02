02d4f602926143749e9d634dfd21df1ba8e708a6
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2021-08-02 22:54:17 +0530

    Cart clearn aaded

--- cart/urls.py
+++ cart/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from cart.views import *
 
 urlpatterns = [
-    path('cart/getcart', GetCart.as_view()),
-    path('cart/additem', AddItem.as_view()),
-    path('cart/removeitem', RemoveItem.as_view()),
+    path('cart/getcart/', GetCart.as_view()),
+    path('cart/additem/', AddItem.as_view()),
+    path('cart/removeitem/', RemoveItem.as_view()),
+    path('cart/clear/', ClearCart.as_view())
 ]
