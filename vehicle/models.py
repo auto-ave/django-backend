b5e63b5c73612a397516879f94c8cc4e2c01b029
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2021-08-25 23:28:22 +0530

    Added wheel thing separately

--- vehicle/models.py
+++ vehicle/models.py
@@ -5,9 +5,17 @@ from vehicle.constants import VEHICLE_MODELS, VEHICLE_TYPES
 
 class VehicleType(Model):
     model = models.CharField(max_length=50, primary_key=True, help_text='Bike, Scooty, Sedan, Hachback, SUV, Truck, Bada Wala Truck')
-    wheel = models.CharField(max_length=50, help_text='Two Wheeler, Three Wheeler, Four Wheeler, Commercial')
+    # wheel = models.CharField(max_length=50, help_text='Two Wheeler, Three Wheeler, Four Wheeler, Commercial')
+    wheel = models.ForeignKey('Wheel', on_delete=models.CASCADE, related_name='vehicle_types', null=True)
     description = models.TextField(blank=True, null=True)
     image = models.URLField(default="https://d3to388m2zu1ph.cloudfront.net/media/questions/g916_1_1.png")
 
     def __str__(self):
         return '{} : {}'.format(self.wheel, self.model)
+
+class Wheel(Model):
+    code = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=50, help_text='Two Wheeler, Three Wheeler, Four Wheeler, Commercial')
+
+    def __str__(self):
+        return self.name
--- vehicle/serializers.py
+++ vehicle/serializers.py
@@ -3,6 +3,12 @@ from rest_framework import serializers
 from vehicle.models import VehicleType
 
 class VehicleTypeSerializer(serializers.ModelSerializer):
+    wheel = serializers.SerializerMethodField()
     class Meta:
         model = VehicleType
         exclude = ('created_at', 'updated_at')
+    
+    def get_wheel(self, obj):
+        if obj.wheel:
+            return obj.wheel.name
+        return None
