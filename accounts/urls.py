7a50511e175479cf74bb44ab179ef15766e55893
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2021-08-29 19:52:06 +0530

    sdkfjdksjf

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('consumer/login/sendOTP/', auth.AuthGetOTP.as_view()),
     path('consumer/login/checkOTP/', auth.AuthCheckOTP.as_view()),
 
-    path('salesman/login/', auth.SalesmanLogin.as_view())
+    path('salesman/login/', auth.SalesmanLogin.as_view()),
+    path('store-owner/login/', auth.SalesmanLogin.as_view())
 ]
