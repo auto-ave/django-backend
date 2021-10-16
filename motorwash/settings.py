013eb072d3bd18f31d845d2aee7c0801f12ae992
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2021-10-16 22:29:37 +0530

    Debug true

--- motorwash/settings.py
+++ motorwash/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e**+b4*p=^8@$%#s(n4+_$v=5#b#e1(jr&5(aq5c9%-z8%bgnb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
