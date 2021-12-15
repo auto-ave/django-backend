c00eda285004a749f2f6cb7b8ccb03b43760b79d
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2021-12-15 22:02:40 +0530

    CORS changes

--- motorwash/settings.py
+++ motorwash/settings.py
@@ -211,21 +211,22 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
     "http://localhost:3000",
+    "http://localhost:4000",
     "http://127.0.0.1:3000",
+    "http://127.0.0.1:4000",
 
     "https://moterwash.netlify.app",
     "https://motorwash-salesman.netlify.app",
     "https://owner-motorwash.netlify.app",
 
-    "https://autoave.care",
-    "https://sales.autoave.care",
-    "https://owner.autoave.care",
+    # "https://autoave.care",
+    # "https://sales.autoave.care",
+    # "https://owner.autoave.care",
 
     "https://autoave.in",
     "https://sales.autoave.in",
     "https://owner.autoave.in",
 
-    ".netlify\.app",
 ]
 
 TWILIO_ACCOUNT_SID = "AC45adcb12c870de63120cacd8c2dd14b8"
