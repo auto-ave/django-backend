5b594f852094ddfb89d9c72c8e2183b4caf37d7d
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2022-01-12 23:55:38 +0530

    Some owner changes, not neccessary but hope not breaks

--- misc/notification_contents.py
+++ misc/notification_contents.py
@@ -100,6 +100,10 @@ def NOTIFICATION_OWNER_NEW_BOOKING(booking):
     store = booking.store
     vehicle_model = booking.vehicle_model
     event = booking.event
+    if store.has_owner():
+        userid = store.owner.user.id
+    else:
+        userid = None
     return {
         'title': 'New booking on {} for {} {}'.format(event.start_datetime.strftime('%d %b - %I:%M %p'), vehicle_model.brand, vehicle_model.model),
         'body': 'Booking for {} {} on {} has been confirmed. Be sure to complete the service on time and collect the due amount displayed on your mobile application.'.format(
@@ -108,7 +112,7 @@ def NOTIFICATION_OWNER_NEW_BOOKING(booking):
             event.start_datetime.strftime('%d %b')
         ),
         'image': random_item(MODI_IMAGES),
-        'userid': store.owner.user.id,
+        'userid': userid,
         'data': {
             'reload': "true"
         }
@@ -119,9 +123,13 @@ def NOTIFICATION_OWNER_BOOKING_INITIATED(booking):
     store = booking.store
     vehicle_model = booking.vehicle_model
     event = booking.event
+    if store.has_owner():
+        userid = store.owner.user.id
+    else:
+        userid = None
     return {
         'title': 'Initiated booking on {} for {} {}'.format(event.start_datetime.strftime('%d %b - %I:%M %p'), vehicle_model.brand, vehicle_model.model),
         'body': 'Booking for {} {} on {} has been confirmed. # TODO: content'.format(vehicle_model.brand, vehicle_model.model, event.start_datetime.strftime('%d %b')),
         'image': random_item(MODI_IMAGES),
-        'userid': store.owner.user.id
+        'userid': userid
     }
