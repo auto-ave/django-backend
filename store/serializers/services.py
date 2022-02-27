bbd12173acd750c9cd01203a2f8eb2701c3a6d28
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2022-02-28 00:26:51 +0530

    Added tags to store service list

--- store/serializers/services.py
+++ store/serializers/services.py
@@ -1,7 +1,5 @@
-from django.db.models import query
-from common.utils import minutes_to_time_string
+from common.serializers import ServiceTagSerializer, StoreServiceListTagSerializer
 from vehicle.models import VehicleType
-import vehicle
 from rest_framework import serializers
 from store.models import PriceTime, Service,Store
 
@@ -9,6 +7,7 @@ class PriceTimeListSerializer(serializers.ModelSerializer):
     service = serializers.SerializerMethodField()
     time_interval = serializers.SerializerMethodField()
     offer = serializers.SerializerMethodField()
+    tags = serializers.SerializerMethodField()
 
     def get_service(self, obj):
         return obj.service_name
@@ -28,6 +27,11 @@ class PriceTimeListSerializer(serializers.ModelSerializer):
                 'code': first_offer.code,
             }
         return None
+
+    def get_tags(self, obj):
+        service = obj.service
+        tags = service.tags.all()
+        return StoreServiceListTagSerializer(tags, many=True).data
 
     class Meta:
         model = PriceTime
